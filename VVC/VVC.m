310d9721782870b979356dcf5723606a0a6375ad
Ruijie Chen <44657154+deoxyribonucleic-acid@users.noreply.github.com>
2023-01-21 00:09:21 +0800

    Add files via upload

--- VVC/VVC.m
+++ VVC/VVC.m
@@ -1,40 +1,41 @@
 clc;clear;
 %global params
-img_wid = 1024; %长宽和数据集大小
-img_hei = 1024;
-data_size = 1875;
-bpp=zeros(1,data_size);
+img_wid = 768; %长宽和数�?�集大�?
+img_hei = 512;
+
 
 %folders
 work_dir='D:\matlab'; %主文件夹目录
-img_dir = '\source\1024\'; %输入的jpg的目录
-yuv_dir = '\recheck\VVC\yuv\'; %jpg转yuv 输出目录
-rec_dir = '\recheck\VVC\dec\'; %VVC压缩后的重构图片(.yuv)的输出目录
-bit_dir = '\recheck\VVC\enc\'; %vvc压缩后的码流（.bin）的输出目录
-fmt_src = '.jpg'; %指定输入输出格式 不要动
+img_dir = '\source\1024\'; %输入的jpg的目�?
+yuv_dir = '\VVC\yuv\'; %jpg转yuv 输出目录
+rec_dir = '\VVC\rec\'; %VVC压缩�?�的�?构图片(.yuv)的输出目�?
+bit_dir = '\VVC\enc\'; %vvc压缩�?�的�?�?�?.bin）的输出目录
+fmt_src = '.png'; %指定输入输出格�? �?�?�?
 fmt_trans = '.yuv';
 fmt_bitstream= '.bin';
 fmt_reconst = '.yuv';
 
 %params for ffmpeg
 fmt='yuv420p';
-ffmpegpath='D:\matlab\JPG2YUV\ffmpeg.exe'; %ffmpeg的路径
+ffmpegpath='D:\matlab\JPG2YUV\ffmpeg.exe'; %ffmpeg的路�?
 
 %params for VVC encode
-vvcpath='D:\matlab\VVC\EncoderApp.exe'; %vvc编解码器的路径
+vvcpath='D:\matlab\VVC\EncoderApp.exe'; %vvc编解�?器的路�?
 decpath='D:\matlab\VVC\DecoderApp.exe';
-cfg_dir ='D:\matlab\VVC\encoder_intra_vtm.cfg'; %vvc配置的路径
-qp='63'; %qp值 这个要改
-
-parfor i=1:data_size %parfor多线程循环，debug的时候改成for
-    %d拼接文件路径
-    img_ind = string(i-1);
+cfg_dir ='D:\matlab\VVC\encoder_intra_vtm.cfg'; %vvc�?置的路�?
+qp='61'; %qp�? 这个�?改
+imgs=dir(convertStringsToChars(strcat(work_dir,img_dir,'*.png')));
+bpp=zeros(1,size(imgs,1));
+%bpp_name=zeros(1,size(imgs,1));
+parfor i=1:size(imgs,1) %parfor多线程循环，debug的时候改�?for
+    %d拼接文件路�
+    [a,img_ind,ext] = fileparts(imgs(i).name);
     imgpath = convertStringsToChars(strcat(work_dir,img_dir,img_ind,fmt_src));
     yuvpath = convertStringsToChars(strcat(work_dir,yuv_dir,img_ind,fmt_trans));
     reconstpath = convertStringsToChars(strcat(work_dir,rec_dir,img_ind,fmt_reconst));
     bitstreampath = convertStringsToChars(strcat(work_dir,bit_dir,img_ind,fmt_bitstream));
     
-    %用ffmpeg把输入的jpg换成yuv
+    %用ffmpeg把输入的jpg�?��?yuv
     if exist(yuvpath,'file')==0 && exist(bitstreampath,'file')==0
         disp(['converting ',imgpath])
         cmd1 = [ffmpegpath,' -i ',imgpath,      ...
@@ -43,7 +44,7 @@ parfor i=1:data_size %parfor多线程循环，debug的时候改成for
         system(cmd1);
     end
     %yuv进行压缩
-    if exist(bitstreampath,'file')==0 %判断：无码流就用vvc编码
+    if exist(bitstreampath,'file')==0 || 1 %判断：无�?�?就用vvc编�?
         cmd2 = [vvcpath,' -i ',yuvpath,                                     ...
                         ' -c ' ,cfg_dir,                                    ...
                         ' -fr ','1',                                        ...
@@ -55,7 +56,7 @@ parfor i=1:data_size %parfor多线程循环，debug的时候改成for
                         ' -o ',reconstpath]
         system(cmd2);
     end
-    %有码流无重构，就用解码器解码
+    %有�?�?无�?构，就用解�?器解�?
     if exist(reconstpath,'file')==0
         cmd3 = [decpath,' -b ',bitstreampath,                               ...
                         ' -o ',reconstpath];
@@ -64,12 +65,14 @@ parfor i=1:data_size %parfor多线程循环，debug的时候改成for
     %算bpp
     disp(['reading from ',bitstreampath])
     storage = dir(bitstreampath);
-    bpp(i) = storage.bytes*8/img_wid/img_hei;
+    bpp(i)= storage.bytes*8/img_wid/img_hei;
     disp(['bpp=',convertStringsToChars(string(bpp(i)))])
+    bpp_name(i)=strcat(img_ind,ext," ",string(bpp(i)));
 end
 %plotting
-id = 1:data_size;
+id = 1:size(imgs,1);
 scatter(id,bpp,2,'filled');
 grid on
 %average bpp calculation
-disp(['average bpp ',convertStringsToChars(string(mean(bpp)))]) %这儿输出平均bpp（控制台）
+disp(['average bpp ',convertStringsToChars(string(mean(bpp)))]) %这儿输出平�?�bpp（控制�?��?
+disp(bpp_name)
